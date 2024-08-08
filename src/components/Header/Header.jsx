8272709d7e8399fc1a80d63456b2169c02bf46ef
PriyeshRai369 <priyeshrai369@gmail.com>
2024-08-08 16:28:12 +0530

    drop down btn added.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -10,6 +10,7 @@ export default function Header() {
   const headerRef = useRef(null);
   const container = useRef();
   const upperHeaderRef = useRef();
+  const [dropDownOpen,setDropOpen] = useState(false)
 
   const handleToggle = () => {
     setIsOpened(!isOpened);
@@ -98,7 +99,7 @@ export default function Header() {
       });
       gsap.from(".btnContainer", {
         delay: 1.2,
-        y:-50,
+        y: -50,
         duration: 0.5,
         opacity: 0,
       });
@@ -125,27 +126,27 @@ export default function Header() {
         </div>
         <div className="btnContainer">
           <button className="loginBtn">Login</button>
-        <div className="menuBtnContainer">
-          <button
-            id="menuBtns"
-            className={isOpened ? "opened menu" : "menu"}
-            aria-expanded={isOpened}
-            onClick={handleToggle}
-            aria-label="Main Menu"
-          >
-            <svg width="50" height="50" viewBox="0 0 100 100">
-              <path
-                className="line line1"
-                d="M 20,29.000046 H 80.000231 C 80.000231,29.000046 94.498839,28.817352 94.532987,66.711331 94.543142,77.980673 90.966081,81.670246 85.259173,81.668997 79.552261,81.667751 75.000211,74.999942 75.000211,74.999942 L 25.000021,25.000058"
-              />
-              <path className="line line2" d="M 20,50 H 80" />
-              <path
-                className="line line3"
-                d="M 20,70.999954 H 80.000231 C 80.000231,70.999954 94.498839,71.182648 94.532987,33.288669 94.543142,22.019327 90.966081,18.329754 85.259173,18.331003 79.552261,18.332249 75.000211,25.000058 75.000211,25.000058 L 25.000021,74.999942"
-              />
-            </svg>
-          </button>
-        </div>
+          <div className="menuBtnContainer">
+            <button
+              id="menuBtns"
+              className={isOpened ? "opened menu" : "menu"}
+              aria-expanded={isOpened}
+              onClick={handleToggle}
+              aria-label="Main Menu"
+            >
+              <svg width="50" height="50" viewBox="0 0 100 100">
+                <path
+                  className="line line1"
+                  d="M 20,29.000046 H 80.000231 C 80.000231,29.000046 94.498839,28.817352 94.532987,66.711331 94.543142,77.980673 90.966081,81.670246 85.259173,81.668997 79.552261,81.667751 75.000211,74.999942 75.000211,74.999942 L 25.000021,25.000058"
+                />
+                <path className="line line2" d="M 20,50 H 80" />
+                <path
+                  className="line line3"
+                  d="M 20,70.999954 H 80.000231 C 80.000231,70.999954 94.498839,71.182648 94.532987,33.288669 94.543142,22.019327 90.966081,18.329754 85.259173,18.331003 79.552261,18.332249 75.000211,25.000058 75.000211,25.000058 L 25.000021,74.999942"
+                />
+              </svg>
+            </button>
+          </div>
         </div>
       </div>
       <nav className="nav" ref={headerRef}>
@@ -181,13 +182,13 @@ export default function Header() {
           </div>
           {/* <div style={{width:"2px",height:"40px",backgroundColor:"black"}}></div> */}
           <div className="lkns">
-            <NavLink
-              to="/engage"
-              className={({ isActive }) => (isActive ? "act" : "navLinks")}
-              onClick={() => setIsOpened(false)}
-            >
-              Engage
-            </NavLink>
+            <div className="navLinks" onClick={()=>setDropOpen(!dropDownOpen)}>
+              Engage <i class="fa-solid fa-angle-right" style={{transform:`${!dropDownOpen ? 'rotate(90deg)' : 'rotate(-90deg)'}`,color:`${!dropDownOpen ?'black': "#f08a5d"}`}}></i>
+            </div>
+              <ul className="dropDownContainer" style={{display:`${dropDownOpen ? 'block': 'none'}` }}>
+                <li>Share Achivements</li>
+                <li>Share Opportunity</li>
+              </ul>
           </div>
           {/* <div style={{width:"2px",height:"40px",backgroundColor:"black"}}></div> */}
           <div className="lkns">
